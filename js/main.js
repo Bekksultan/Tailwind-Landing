189d1687802922d5ff584f27d015cd1ca07dcc5b
Beksultan_A <31516770+Bekksultan@users.noreply.github.com>
2023-12-31 21:20:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* Accordion */
+const accordionHeader = document.querySelectorAll(".accordion-header");
+accordionHeader.forEach((header) => {
+header.addEventListener("click", function () {
+    const accordionContent = header.parentElement.querySelector(".accordion-content");
+    let accordionMaxHeight = accordionContent.style.maxHeight;
+
+    // Condition handling
+    if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
+    accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32}px`;
+    header.querySelector(".fas").classList.remove("fa-plus");
+    header.querySelector(".fas").classList.add("fa-minus");
+    header.parentElement.classList.add("bg-indigo-50");
+    } else {
+    accordionContent.style.maxHeight = `0px`;
+    header.querySelector(".fas").classList.add("fa-plus");
+    header.querySelector(".fas").classList.remove("fa-minus");
+    header.parentElement.classList.remove("bg-indigo-50");
+    }
+});
+});
+/* Accordion */
+$('#showMore').click(function() {
+    $('#boxs .box:hidden').slice(0,3).slideDown();
+    if ($("#boxs .box:hidden").length == 0) {
+      $("#showMore").fadeOut('slow');
+     }
+   });
+
+   
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      backgroundImage:{
+        'header-main': "url('../../assets/header.png')",
+        'form-featur': "url(../../assets/Call to Action.png)"
+
+      },
+
+      colors: {
+        'regal-blue': '#243c5a',
+        'dark-blue': '#0A2640',
+        'faq-gray': '#C4C4C4',
+        'gray': '#777777',
+        'light-green': '#65E4A3',
+        'feature-green': '#4FE9A4'
+
+      },
+    },
+  },
+  plugins: [],
+}
+
+  
